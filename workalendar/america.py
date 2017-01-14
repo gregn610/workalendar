c65adac32109f9b6af90e8693279dd840d5594f9
Greg N <gregn@fastmail.com>
2017-01-14 19:50:36 +0100

    PA Carnival Monday

--- workalendar/america.py
+++ workalendar/america.py
@@ -211,6 +211,9 @@ class Panama(WesternCalendar, ChristianMixin):
     def get_variable_days(self, year):
         days = super(Panama, self).get_variable_days(year)
         days.append(
-            (self.get_ash_wednesday(year) - timedelta(days=1), "Carnival")
+            (self.get_ash_wednesday(year) - timedelta(days=2), "Carnival Monday")
+        )
+        days.append(
+            (self.get_ash_wednesday(year) - timedelta(days=1), "Carnival Tuesday")
         )
         return days
